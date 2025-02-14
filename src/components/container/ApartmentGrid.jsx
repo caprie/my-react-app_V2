40af32a4342b6692caf191b9ac9357155d4ccee5
caprie <caprie@live.fr>
2025-02-14 23:25:09 +0100

    home avec photos ok

--- src/components/container/ApartmentGrid.jsx
+++ src/components/container/ApartmentGrid.jsx
@@ -1,31 +1,53 @@
-import React from 'react';
-import './apartmentGrid.scss';
+import React from 'react'
+import './apartmentGrid.scss'
+import balloon from '../../assets/balloon.jpg' 
+import big_space from '../../assets/big_space.jpg'
+import bubbles from '../../assets/bubbles.jpg'
+import chalet_blanc from '../../assets/chalet_blanc.jpg'
+import class_fire from '../../assets/class_fire.jpg'
+import dot_best from '../../assets/dot_best.jpg'
+import dots_and_dots from '../../assets/dots_and_dots.jpg'
+import la_tete_dans_les_nuages from '../../assets/la_tete_dans_les_nuages.jpg'
+import little_house from '../../assets/little_house.jpg'
+import marocco from '../../assets/marocco.jpg'
+import clouds from '../../assets/clouds.jpg'
+import steampunk from '../../assets/steampunk.jpg'
+import wood_fire from '../../assets/wood_fire.jpg'
 
-// simulation liste d'apparts
+// liste des apparts avec images
 const apartments = [
-  { id: 1, title: 'Appartement Cozy', image: '/img1.jpg' },
-  { id: 2, title: 'Loft Moderne', image: '/img2.jpg' },
-  { id: 3, title: 'Studio Charmant', image: '/img3.jpg' },
-  { id: 4, title: 'Villa Bord de Mer', image: '/img4.jpg' },
-  { id: 5, title: 'Appartement Luxe', image: '/img5.jpg' },
-  { id: 6, title: 'Maison de Campagne', image: '/img6.jpg' }
+  { id: 1, title: 'Appartement duplex Les Pieds dans l eau', image: balloon },
+  { id: 2, title: 'Appartement y a de l espace', image: big_space },
+  { id: 3, title: 'Appartement Mars Attack', image: bubbles },
+  { id: 4, title: 'Suite Nuptiale', image: chalet_blanc },
+  { id: 5, title: 'Appartement SPA', image: class_fire },
+  { id: 6, title: 'Appartement dans les nuages', image: clouds },
+  { id: 7, title: 'Maison du point', image: dot_best },
+  { id: 8, title: 'Appartement Perds pas la boule', image: dots_and_dots },
+  { id: 9, title: 'Maison la tête dans les nuages', image: la_tete_dans_les_nuages },
+  { id: 10, title: 'Maison tout comme une grande', image: little_house },
+  { id: 11, title: 'Maison inspiration', image: marocco },
+  { id: 12, title: 'Maison steampunk', image: steampunk },
+  { id: 13, title: 'Beau feu de bois', image: wood_fire }
 ];
 
 const ApartmentGrid = () => {
+  console.log(apartments); // Vérifie si les images sont bien chargées
+
   return (
     <div className="apartment-grid">
       {apartments.map(apartment => (
         <div key={apartment.id} className="apartment-card">
           <img
-            src={apartment.image}
+             src={apartment.image ? apartment.image : "https://via.placeholder.com/150"}
             alt={apartment.title}
             className="apartment-image"
           />
-          <h3>{apartment.title}</h3>
+          <div className="apartment-title">{apartment.title}</div>
         </div>
       ))}
     </div>
   );
 };
 
-export default ApartmentGrid;
+export default ApartmentGrid
